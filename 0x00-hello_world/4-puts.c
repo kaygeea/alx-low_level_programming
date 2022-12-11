39424d3e49545d48adbbddc35e83a9d4f90878c7
Koyejo <adinlewakoyejo@yahoo.com>
2022-12-11 03:01:54 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void)
+{
+	//string inisialisation
+	char Print[] = "Programming is like building a multilingual puzzle";
+
+	puts(Print); //writing the string to stdout
+
+	return 0;
+}
