f05265c968deb2b1a8a0a608aa4559e6040b62af
Koyejo <adinlewakoyejo@yahoo.com>
2022-12-12 17:35:14 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Retun: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
