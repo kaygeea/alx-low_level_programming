dbaa6d1ac3ad24ee77219c57156e3427126636d9
Koyejo <adinlewakoyejo@yahoo.com>
2022-12-13 09:32:40 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char a = 'a' , b;
+	while (a <= 'z')
+	{
+		b = 1;
+		while (b <= 10)
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
