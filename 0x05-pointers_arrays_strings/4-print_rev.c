e0b16805440984b0c40ca4792fad64d0e659bca4
Koyejo <adinlewakoyejo@yahoo.com>
2022-12-20 14:55:26 -0800

    Task 5 - A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - prints a string in reverse order
+ *@s: value of pointer
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
