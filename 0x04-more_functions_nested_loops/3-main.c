e00c6eb18b1b4c7ed8653b9e2602432929464389
Koyejo <adinlewakoyejo@yahoo.com>
2022-12-31 11:43:00 +0100

    Task 0x04 files

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * #include <stdio.h>
+ */
+int main(void)
+{
+	print_numbers();
+	return (0)
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+
+#endif /* MAIN_H */
