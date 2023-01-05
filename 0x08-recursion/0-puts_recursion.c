e3c63dd95d78d8c56eb0319397b4e3ed83ca26e5
Koyejo <adinlewakoyejo@yahoo.com>
2023-01-05 09:40:43 -0800

    Task 2 - Dreams feel real while we are in them. It is only when we wake up that we realize something was actually strange

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,8 +7,7 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-		_puts_recursion(s + 1);
-		_putchar (*s);
-	}
+		return;
+	_puts_recursion(s + 1);
+	_putchar (*s);
 }
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: incoming argument
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else return 1 + _strlen_recursion(s + 1);
+}
