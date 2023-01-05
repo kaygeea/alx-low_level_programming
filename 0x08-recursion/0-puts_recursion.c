59713263cd828f6f826a51f1a2f453022d0b1faf
Koyejo <adinlewakoyejo@yahoo.com>
2023-01-05 08:07:11 -0800

    Task 0 - She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string with recursion
+ * @s: incoming argument
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_puts_recursion(s + 1);
+		_putchar (*s);
+	}
+}
