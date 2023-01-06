1f509d120ed7ba4d4841fece83e903b54059ccec
Koyejo <adinlewakoyejo@yahoo.com>
2023-01-05 16:53:11 -0800

    Task 4 - You must not be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: incoming argument
+ *
+ * Return: natural square root on n, or -1 if impossible
+ */
+int _sqrt_recursion(int n)
+{
+	if (n / n != 0)
+		return (-1);
+	else
+		return ()
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number, 
+ * otherwise return 0
+ * @n: incoming argument
+ *
+ * Return: 1 if value of n is prime number, otherwise return 0
+ */
+int is_prime_number(int n)
+	
