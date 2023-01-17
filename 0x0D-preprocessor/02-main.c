30c590b1bce6ea477419ac74820d8e72cdcf6186
Koyejo <adinlewakoyejo@yahoo.com>
2023-01-16 18:49:15 -0800

    Task 3 - File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * File name - a program that prints the name of the file it was compiled from
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
