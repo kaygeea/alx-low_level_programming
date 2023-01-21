c7068816388db0c6957561319f0fd4a40ef4db7b
Koyejo <adinlewakoyejo@yahoo.com>
2023-01-21 01:33:35 -0800

    Task 0 - What is my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name and to be called back
+ * @name: incoming name argument for pointer to name
+ * @f: function pointer
+ *
+ * Return: Void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
