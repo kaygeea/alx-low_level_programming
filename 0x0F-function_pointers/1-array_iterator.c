d8e0f6e1328e61950780fc142449556fa9576761
Koyejo <adinlewakoyejo@yahoo.com>
2023-01-21 02:00:32 -0800

    Task 1 - If you spend too much time thinking about a thing, you will never get it done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - program that iterates through array and executes functions
+ * @array: incoming argument for array to be iterated through
+ * @size: size of incoming array argument
+ * @action: pointer to function to be executed on iteration
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
