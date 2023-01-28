9419371ee649991b1cb61cb98c0eda26a25f9367
Koyejo <adinlewakoyejo@yahoo.com>
2023-01-28 10:31:06 -0800

    Task 4 - strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,20 +10,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int value, i = 0;
+	int i, value;
+
+	i = 0;
+
+	while (s1[i] == s2[i] && s1[i] != '\0')
+	{
+		i++;
+	}
 
 	value = s1[i] - s2[i];
-
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-		{
-			value = 1;
-			break;
-		}
-	}
-	if (value == 1)
-		return (value);
-	else
-		return (value);
+	return (value);
 }
