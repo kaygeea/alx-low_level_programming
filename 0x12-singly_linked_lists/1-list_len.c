133fc9efc26dd381eb409a459f5e3a983d4469a6
Koyejo <adinlewakoyejo@yahoo.com>
2023-01-29 07:53:41 -0800

    Task 1 - Write a function that returns the number of elements in a linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,17 +11,14 @@ size_t list_len(const list_t *h)
 {
 	unsigned int count = 1;
 
-	while (h->next != NULL)
+	if (!(h))
 	{
-		if (h->len == 0)
-		{
-			return (0);
-		}
-		else
-		{
-			count++;
-			h = h->next;
-		}
+		return (0);
+	}
+	else if (h)
+	{
+		count++;
+		h = h->next;
 	}
 	return (count);
 }
