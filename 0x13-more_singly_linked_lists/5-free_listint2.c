957613ff742af5b7b30543df42a7a4966f634abf
Koyejo <adinlewakoyejo@yahoo.com>
2023-01-31 14:24:31 -0800

    Task 5 - Write a function that frees a linked list and sets head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - a function that frees a linked list
+ * @head: a pointer to the linked list to be free
+ *
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+	{
+		return;
+	}
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
