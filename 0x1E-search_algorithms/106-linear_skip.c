56863455ce479db04982258733ca0015343b6d86
Koyejo <adinlewakoyejo@yahoo.com>
2023-07-16 11:39:16 -0700

    Added code to find val in a sorted skip list of ints

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - Searches for a value in an sorted skip list of integers.
+ *
+ * @list: Incoming arg for a pointer to the head of skip list to be searched.
+ * @value: Incoming argument for value to be searched for in list.
+ *
+ * Return: 1st index where value is located, or NULL otherwise.
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *node, *jump;
+
+	if (list == NULL)
+		return (NULL);
+
+	for (node = jump = list; jump->next != NULL && jump->n < value;)
+	{
+		node = jump;
+		if (jump->express != NULL)
+		{
+			jump = jump->express;
+			printf("Value checked at index [%ld] = [%d]\n",
+					jump->index, jump->n);
+		}
+		else
+		{
+			while (jump->next != NULL)
+				jump = jump->next;
+		}
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
+
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
+}
