49095bcdb1e4c79235b9d77be3a10141acbe8e1e
Koyejo <adinlewakoyejo@yahoo.com>
2023-07-16 11:32:45 -0700

    Added code to find val in linked list with jump search algo

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - Uses a Jump Search Algorithm (JSA) to search for a value
+ *             in an sorted list of integers.
+ *
+ * @list: Incoming arg for a pointer to the head of list to be searched.
+ * @size: Incoming argument for number of nodes in the list to be searched.
+ * @value: Incoming argument for value to be searched for in list.
+ *
+ * Return: 1st index where value is located, or -1 otherwise.
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t step, step_size;
+	listint_t *node, *jump;
+
+	if (list == NULL || size == 0)
+		return (NULL);
+
+	step = 0;
+	step_size = sqrt(size);
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
+	{
+		node = jump;
+		for (step += step_size; jump->index < step; jump = jump->next)
+		{
+			if (jump->index + 1 == size)
+				break;
+		}
+		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
+
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
+}
